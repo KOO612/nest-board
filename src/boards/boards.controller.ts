408576de6e363f1071d29e39f9826eae7a9689a0
KOO612 <ksm99007@gmail.com>
2023-12-06 15:12:53 +0900

    따라하면서 배우는 NestJS

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Board } from './boards.model';
+
+@Controller('boards')
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  // 모든 게시물을 가져오는 handler
+  @Get()
+  getAllTask(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardsService.createBoard(title, description);
+  }
+}
