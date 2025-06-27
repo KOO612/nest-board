addbb3210b236647feac877ace51f4a305e53467
KOO612 <ksm99007@gmail.com>
2025-06-27 17:30:58 +0900

    커스텀 데코레이터

--- simple-board/src/app.controller.ts
+++ simple-board/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IP } from './decorators/ip.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@IP() ip: string): string {
+    console.log(ip);
     return this.appService.getHello();
   }
 
